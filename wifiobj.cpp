d2ed3af763a6c9d908899691b19aa6b8df5d4d1f
Stoyanov <stoyanov.karlovo@gmail.com>
2023-04-25 20:13:32 +0300

    set password wrongly intentionally

--- wifiobj.cpp
+++ wifiobj.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-	WirelessConnectionManager wirelessConnectionManager("Vakrilov04", "04051990");
+	WirelessConnectionManager wirelessConnectionManager("Vakrilov04", "04051999");
 	return 0;
 }
