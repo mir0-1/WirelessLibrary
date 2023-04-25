3ce4fa2297ee8ff645fe31463804f2718f1cb657
Stoyanov <stoyanov.karlovo@gmail.com>
2023-04-25 19:09:45 +0300

    temporary remove timeout

--- WirelessConnectionManager.cpp
+++ WirelessConnectionManager.cpp
@@ -113,16 +113,16 @@ bool WirelessConnectionManager::activateAndOrAddConnection(NMConnection* connect
 	NMActiveConnectionState connectionState = (*(NMActiveConnectionState*)asyncTransferUnit.extraData);
 	gulong signalHandlerId = g_signal_connect(activatingConnection, "notify::" NM_ACTIVE_CONNECTION_STATE, G_CALLBACK(connectionActivateReadyCallback), (gpointer)&asyncTransferUnit);
 	timeout = 300 * G_USEC_PER_SEC;
-	GSource* gTimeoutSource = g_timeout_source_new(timeout);
-	g_source_set_callback(gTimeoutSource, connectionActivateTimeoutCallback, (gpointer)&asyncTransferUnit, NULL);
-	g_source_attach(gTimeoutSource, gMainContext);
+	//GSource* gTimeoutSource = g_timeout_source_new(timeout);
+	//g_source_set_callback(gTimeoutSource, connectionActivateTimeoutCallback, (gpointer)&asyncTransferUnit, NULL);
+	//g_source_attach(gTimeoutSource, gMainContext);
 	waitForAsync();
 	g_signal_handler_disconnect(activatingConnection, signalHandlerId);
 	
 	bool successful = (connectionState == NM_ACTIVE_CONNECTION_STATE_ACTIVATED);
 	
-	g_source_destroy(gTimeoutSource);
-	g_source_unref(gTimeoutSource);
+	//g_source_destroy(gTimeoutSource);
+	//g_source_unref(gTimeoutSource);
 	
 	return successful;
 }
